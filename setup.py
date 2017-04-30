d383021a7aa48485a12a3eb921447a7c27a0ba2c
Tom Wiesing <tkw01536@gmail.com>
2017-04-30 18:44:04 +0200

    Bugfix setup.py
    This commit bugfixes the setup.py script and fixes potential issues
    arising upon installation.

--- setup.py
+++ setup.py
@@ -5,7 +5,6 @@ from setuptools import setup, find_packages
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-
 setup(
     name="git_manager",
     version="0.0.3",
@@ -14,7 +13,8 @@ setup(
     author="Tom Wiesing",
     author_email="tkw01536@gmail.com",
 
-    py_modules=find_packages(),
+
+    packages=find_packages(),
     scripts=['git-manager'],
 
     description=("Manages multiple git repositories"),

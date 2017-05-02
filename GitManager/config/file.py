6ddb6388aa95a31151b5c368338ea15874e13b09
Tom Wiesing <tkw01536@gmail.com>
2017-05-02 16:36:20 +0200

    Fix regression in File.read()
    This commit fixes a regression in the read() method of the File() class
    that prevented repositories to be read from the configuration file
    properly.

--- GitManager/config/file.py
+++ GitManager/config/file.py
@@ -18,7 +18,7 @@ class File(tree.Tree):
         """ Re-reads the lines currently contained in this file """
 
         with open(self.__fn, "r") as fp:
-            self.__lines = [line.ConfigLine.parse(l) for l in fp.readlines()]
+            self.lines = [line.ConfigLine.parse(l) for l in fp.readlines()]
 
     def write(self):
         """ Writes the lines currently contained in this file to disk """
